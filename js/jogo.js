3421ef45052932b5ca96728c7ad37dba9df799bb
Thiago Silveira <thiagosilveira.ifc@gmail.com>
2025-05-20 09:20:21 -0300

    Minha versão estilizada, com memes do floptok. contém 4 cartas, e 100 tentativas.

--- js/jogo.js
+++ js/jogo.js
@@ -8,6 +8,12 @@
     const btnReiniciar = document.getElementById('reiniciar');
     const btnJogarNovamente = document.getElementById('joganovamente');
 
+    const telaAcerto = document.getElementById('telaAcerto');
+    const telaErro = document.getElementById('telaErro');
+    const mensagemFinal = document.getElementById('mensagemFinal');
+
+
+
     //funçao que zera os valores das variáveis controladoras
     function reiniciar() {
       desempenho = 0;
@@ -24,14 +30,21 @@
 
     //funçao jogar novamente
     function jogarNovamente() {
+      telaAcerto.className = 'mensagem invisivel';
+      telaErro.className = 'mensagem invisivel';
+      // Remove todas as imagens de erro anteriores
+      let imagensErro = document.querySelectorAll('#imagemErro');
+      imagensErro.forEach(img => img.remove());
+
+
       jogar = true;//variável jogar volta a ser verdadeira
       //armazenamos todas as div na variável divis (getElementsByTagName)
       let divis = document.getElementsByTagName("div");
       //percorremos todas as divs armazenadas
       for (i = 0; i < divis.length; i++) {
-        //verificamos se sao as divs com ids 0 ou 1 ou 2
-        if (divis[i].id == 0 || divis[i].id == 1 || divis[i].id == 2) {
-          //alteramos a classe css das divs 0, 1 e 2 (className)
+        //verificamos se sao as divs com ids 0 ou 1 ou 2 ou 3
+        if (divis[i].id == 0 || divis[i].id == 1 || divis[i].id == 2 || divis[i].id == 3) {
+          //alteramos a classe css das divs 0, 1, 2 e 3 (className)
           divis[i].className = "inicial";
         }
       }
@@ -50,7 +63,7 @@
       //calcula o desempenho em porcentagem
       desempenho = (acertos / tentativas) * 100;
       //escreve o placar com os valores atualizados (innerHTML)
-      document.getElementById("resposta").innerHTML = "Placar - Acertos: " + acertos + " Tentativas: " + tentativas + " Desempenho: " + Math.round(desempenho) + "%";
+      document.getElementById("resposta").innerHTML = "Placar - Sayonaras encontradas: " + acertos + " Tentativas: " + tentativas + " Reputação: " + Math.round(desempenho) + "%";
 
     }
 
@@ -62,7 +75,7 @@
       const img = new Image(100);
       img.id = "imagem";
       //altera o atributo src (source) da imagem criada
-      img.src = "https://upload.wikimedia.org/wikipedia/commons/2/2e/Oxygen480-emotes-face-smile-big.svg";
+      img.src = "sayonara.png";
       //adiciona a imagem criada na div (obj) escolhida pelo jogador (appendChild)
       obj.appendChild(img);
     }
@@ -75,29 +88,37 @@
         jogar = false;
         //incrementa as tentativas
         tentativas++;
-        //verifica se jogou 3 vezes
-        if (tentativas == 3) {
+        //verifica se jogou 100 vezes
+        if (tentativas == 100) {
           //oculta o botao joganovamente alterando a classe css (getElementById e className)
           btnJogarNovamente.className = 'invisivel';
           //mostra o botao reiniciar alterando a classe css (getElementById e className)
           btnReiniciar.className = 'visivel';
         }
         //a variável sorteado recebe um valor inteiro (Math.floor) aleatório (Math.random)
-        let sorteado = Math.floor(Math.random() * 3);
+        let sorteado = Math.floor(Math.random() * 4);
         //se o id da <div> escolhida pelo jogador for igual ao número sorteado
         if (obj.id == sorteado) {
-          //chama a funçao acertou passando a div escolhida pelo jogador
-          acertou(obj);
-          //incrementa o contador de acertos
-          acertos++;
-        } else {//se errou a tentativa
-          //altera a classe da <div> escolhida pelo jogador para a classe errou
-          obj.className = "errou";
-          //armazena a div aonde Smile está escondido (getElementById)
-          const objSorteado = document.getElementById(sorteado);
-          //chama a funçao acertou para mostrar a div aonde está o Smile
-          acertou(objSorteado);
-        }
+        acertou(obj);
+        acertos++;
+        telaAcerto.className = 'mensagem visivel';
+        telaErro.className = 'mensagem invisivel';
+      } else {
+        obj.className = "errou";
+
+        // Cria uma imagem de erro e adiciona na carta errada
+        const imgErro = new Image(100);
+        imgErro.src = "https://thumbs.dreamstime.com/z/sapo-um-mutante-est%C3%A1-diante-de-voc%C3%AA-metade-do-e-humano-esta-criatura-verde-humor%C3%ADstica-humanoide-parece-algo-bem-vis%C3%ADvel-filme-219231620.jpg?ct=jpeg"; // nome da sua imagem de erro
+        imgErro.id = "imagemErro"; // um ID para facilitar a remoção depois
+        obj.appendChild(imgErro);
+
+        // Mostra onde estava o smile
+        const objSorteado = document.getElementById(sorteado);
+        acertou(objSorteado);
+        telaErro.className = 'mensagem visivel';
+        telaAcerto.className = 'mensagem invisivel';
+}
+
         //chama a funçao que atualiza o placar
         atualizaPlacar(acertos, tentativas);
       } else {//se o jogador clicar em outra carta sem reiniciar o jogo, recebe um alerta
